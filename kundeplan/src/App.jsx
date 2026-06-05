97188befa7400d2a13266be81430ed0e62f45d40
Ernst Magne Carlsen <ernst.magne.carlsen@atea.no>
2026-06-05 15:02:28 +0200

    arrow fix

--- kundeplan/src/App.jsx
+++ kundeplan/src/App.jsx
@@ -1721,8 +1721,8 @@ function App({ auth = { enabled: false, activeAccount: null, signOut: null, publ
             >
             <svg viewBox={`0 0 ${graph.width} ${graph.height}`} preserveAspectRatio="xMinYMin meet" style={{ width: `${graph.width}px`, height: `${graph.height}px` }} aria-hidden="true">
               <defs>
-                <marker id="arrow" viewBox="0 0 10 10" refX="8" refY="5" markerWidth="7" markerHeight="7" orient="auto-start-reverse">
-                  <path d="M 0 0 L 10 5 L 0 10 z" fill="#243046" />
+                <marker id="arrow" viewBox="0 0 10 10" refX="8" refY="5" markerWidth="5" markerHeight="5" orient="auto-start-reverse">
+                  <path d="M 0 0 L 10 5 L 0 10 z" fill="context-stroke" />
                 </marker>
               </defs>
               <g strokeLinecap="round" strokeLinejoin="round">
