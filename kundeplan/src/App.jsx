449eeff6600fb679ce6b11d87ec9f57fbc51cbe4
Ernst Magne Carlsen <ernst.magne.carlsen@atea.no>
2026-06-09 13:43:18 +0200

    fix export

--- kundeplan/src/App.jsx
+++ kundeplan/src/App.jsx
@@ -1747,8 +1747,17 @@ function App({ auth = { enabled: false, activeAccount: null, signOut: null, publ
             >
             <svg viewBox={`0 0 ${graph.width} ${graph.height}`} preserveAspectRatio="xMinYMin meet" style={{ width: `${graph.width}px`, height: `${graph.height}px` }} aria-hidden="true">
               <defs>
-                <marker id="arrow" viewBox="0 0 10 10" refX="8" refY="5" markerWidth="5" markerHeight="5" orient="auto-start-reverse">
-                  <path d="M 0 0 L 10 5 L 0 10 z" fill="context-stroke" />
+                <marker id="arrow-source" viewBox="0 0 10 10" refX="8" refY="5" markerWidth="5" markerHeight="5" orient="auto-start-reverse">
+                  <path d="M 0 0 L 10 5 L 0 10 z" fill="rgb(42, 152, 134)" />
+                </marker>
+                <marker id="arrow-dependency" viewBox="0 0 10 10" refX="8" refY="5" markerWidth="5" markerHeight="5" orient="auto-start-reverse">
+                  <path d="M 0 0 L 10 5 L 0 10 z" fill="rgb(255, 161, 67)" />
+                </marker>
+                <marker id="arrow-highlighted" viewBox="0 0 10 10" refX="8" refY="5" markerWidth="5" markerHeight="5" orient="auto-start-reverse">
+                  <path d="M 0 0 L 10 5 L 0 10 z" fill="#1d9c8f" />
+                </marker>
+                <marker id="arrow-preview" viewBox="0 0 10 10" refX="8" refY="5" markerWidth="5" markerHeight="5" orient="auto-start-reverse">
+                  <path d="M 0 0 L 10 5 L 0 10 z" fill="#ff5f92" />
                 </marker>
               </defs>
               <g strokeLinecap="round" strokeLinejoin="round">
